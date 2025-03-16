b50efd01879a01ee54b00cd00825b4a53d8f601e
ango06 <143280698+ango06@users.noreply.github.com>
2025-03-15 22:06:59 -0400

    Completed exercise4.js

--- exercises/exercise4.js
+++ exercises/exercise4.js
@@ -17,16 +17,20 @@ const dogs = [
 ];
 
 // Use forEach to print out the name of each dog to the console
-
+dogs.forEach(dog => console.log(dog));
 
 // Use filter AND map to create a new array that contains the names of
 // the dogs that are 'Kind'.
-const kindDogs = null;
+const kindDogs = dogs.filter(dog => dog.temperament.includes("Kind"));
+kindDogs.map(dog => dog.name);
 console.log(kindDogs); 
 // -> array of kind dog objects
 
 // Use map to create a new array identical to the original,
 // but with the id's of the dogs starting at dog-0 instead of dog-1
-const reIdifiedDogs = null;
+const reIdifiedDogs = dogs.map((dog, index) => ({
+    ...dog,
+    id: `dog-${index}`
+}));
 console.log(reIdifiedDogs);
 // -> array of dog objects with the id starting at 0
